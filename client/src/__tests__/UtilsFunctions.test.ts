96cbdb6d303333dee90fb6b8ccf677b8c4e9c16f
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-06-16 12:40:10 +0200

    cleanup

--- client/src/__tests__/UtilsFunctions.test.ts
+++
@@ -1,7 +0,0 @@
-import { getFormattedDate } from '@/utils/date';
-
-test('Tranform data', () => {
-  const date = new Date(1655234340000);
-  const dateOutput = getFormattedDate(date);
-  expect(dateOutput).toBe('2022-06-14');
-});
