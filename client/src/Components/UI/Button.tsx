111dbc96fae46223dd2eea696fe60225a240fe28
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-06-17 08:34:00 +0200

    Custom button

---
+++ client/src/Components/UI/Button.tsx
@@ -0,0 +1,46 @@
+import { GlobalStyles } from '@/Constants/styles';
+import { FC } from 'react';
+import { View, Text, Pressable, GestureResponderEvent, StyleSheet } from 'react-native';
+
+interface Button {
+  mode?: string;
+  style?: Object;
+  onPress: (event: GestureResponderEvent) => void;
+}
+
+const Button: FC<Button> = ({ children, mode, style, onPress }) => {
+  return (
+    <View style={style}>
+      <Pressable onPress={onPress} style={({ pressed }) => pressed && styles.pressed}>
+        <View style={[styles.button, mode === 'flat' && styles.flat]}>
+          <Text style={[styles.buttonText, mode === 'flat' && styles.flatText]}>{children}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary500
+  },
+  flat: {
+    backgroundColor: 'transparent'
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary200
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4
+  }
+});
