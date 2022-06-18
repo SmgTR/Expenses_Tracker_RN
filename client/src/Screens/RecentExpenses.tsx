7405fe3bb082811a5c96caf943344026466a4c46
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-06-18 09:01:39 +0200

    Filter and display last 7 day expenses

--- client/src/Screens/RecentExpenses.tsx
+++ client/src/Screens/RecentExpenses.tsx
@@ -1,7 +1,25 @@
+import { useAppSelector } from '@/redux/hooks';
+import { getDateMinusDays } from '@/utils/date';
 import { ExpensesOutput } from '../Components';
 
 const RecentExpenses = () => {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+  const allExpenses = useAppSelector((state) => state.expenses);
+
+  const recentExpenses = allExpenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today.toString(), 7);
+
+    const expenseDate = new Date(expense.date);
+
+    return expenseDate >= date7DaysAgo && expenseDate <= today;
+  });
+  return (
+    <ExpensesOutput
+      expensesPeriod="Last 7 Days"
+      expensesList={recentExpenses}
+      fallbackText={'No expenses registered for the last 7 days'}
+    />
+  );
 };
 
 export default RecentExpenses;
