b70dad03e733c42e0b97428987bc6f2464506f74
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-06-18 08:59:11 +0200

    update date format

--- client/src/redux/slices/expenses-slice.ts
+++ client/src/redux/slices/expenses-slice.ts
@@ -3,39 +3,48 @@ import { createSlice } from '@reduxjs/toolkit';
 import { Expense } from '@/types/Expense';
 
 const DUMMY_EXPENSES: Expense[] = [
-  { id: 'e1', description: 'A pair of shoes', amount: 59.99, date: new Date('2021-12-19') },
-  { id: 'e2', description: 'Bananas', amount: 39.29, date: new Date('2022-06-08') },
-  { id: 'e3', description: 'Book', amount: 12.42, date: new Date('2022-06-07') },
-  { id: 'e4', description: 'Lightsaber', amount: 145.55, date: new Date('2022-05-21') },
-  { id: 'e5', description: 'Lego', amount: 421.12, date: new Date('2022-06-10') },
-  { id: 'e6', description: 'Coconut', amount: 16.21, date: new Date('2022-06-10') },
-  { id: 'e7', description: 'Iphone', amount: 369.12, date: new Date('2022-06-10') },
-  { id: 'e8', description: 'TV', amount: 4221.86, date: new Date('2022-06-10') },
-  { id: 'e9', description: 'Star Trek Movie', amount: 9.23, date: new Date('2022-06-10') },
-  { id: 'e10', description: 'Chewbacca Cosplay Suit', amount: 421.12, date: new Date('2022-06-10') }
+  { id: 'e1', description: 'A pair of shoes', amount: 59.99, date: 'Sat Jun 18 2022' },
+  { id: 'e2', description: 'Bananas', amount: 39.29, date: 'Saturday, 18 June 2022 05:44:30' },
+  { id: 'e3', description: 'Book', amount: 12.42, date: 'Saturday, 18 June 2022 05:44:30' },
+  { id: 'e4', description: 'Lightsaber', amount: 145.55, date: 'Saturday, 18 June 2022 05:44:30' },
+  { id: 'e5', description: 'Lego', amount: 421.12, date: 'Saturday, 18 June 2022 05:44:30' },
+  { id: 'e6', description: 'Coconut', amount: 16.21, date: 'Saturday, 18 June 2022 05:44:30' },
+  { id: 'e7', description: 'Iphone', amount: 369.12, date: 'Saturday, 18 June 2022 05:44:30' },
+  { id: 'e8', description: 'TV', amount: 4221.86, date: 'Saturday, 18 June 2022 05:44:30' },
+  {
+    id: 'e9',
+    description: 'Star Trek Movie',
+    amount: 9.23,
+    date: 'Saturday, 18 June 2022 05:44:30'
+  },
+  {
+    id: 'e10',
+    description: 'Chewbacca Cosplay Suit',
+    amount: 421.12,
+    date: 'Saturday, 18 June 2022 05:44:30'
+  }
 ];
 
-const initialState = {
-  expenses: DUMMY_EXPENSES
-};
+const initialState: Expense[] = [];
 
 export const expensesSlice = createSlice({
   name: 'expenses',
   initialState,
   reducers: {
     addExpense: (state, { payload }) => {
-      state.expenses.push(payload);
+      state.push(payload);
     },
     updateExpense: (state, { payload }) => {
-      const expenseItem = state.expenses.findIndex((expense) => expense.id === payload.id);
-      state.expenses[expenseItem] = { expenseItem, ...payload };
+      const updatableExpenseIndex = state.findIndex((expense) => expense.id === payload.id);
+      const updatableExpense = state[updatableExpenseIndex];
+      state[updatableExpenseIndex] = { ...updatableExpense, ...payload.expenseChanges };
     },
     deleteExpense: (state, { payload }) => {
-      state.expenses.filter((expense) => expense.id !== payload);
+      return state.filter((expense) => expense.id !== payload.expenseId);
     }
   }
 });
 
-export const { addExpense } = expensesSlice.actions;
+export const { addExpense, updateExpense, deleteExpense } = expensesSlice.actions;
 
 export default expensesSlice.reducer;
--- client/src/utils/date.ts
+++ client/src/utils/date.ts
@@ -1,5 +1,11 @@
-export function getFormattedDate(date: Date) {
+export function getFormattedDate(timestamp: string | Date) {
+  const date = new Date(timestamp);
   const monthBefore = date.getMonth() < 10 ? '0' : '';
   const dayBefore = date.getDate() < 10 ? '0' : '';
   return `${date.getFullYear()}-${monthBefore}${date.getMonth() + 1}-${dayBefore}${date.getDate()}`;
+}
+
+export function getDateMinusDays(timestamp: string | Date, days: number) {
+  const date = new Date(timestamp);
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
 }
