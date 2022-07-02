c8e5298834ea536366dfe4cf0767ecb426257641
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-07-02 12:48:28 +0200

    axios getall and create

--- client/src/redux/services/expense.ts
+++ client/src/redux/services/expense.ts
@@ -1,8 +1,22 @@
-import { allExpenses } from '../slices/expenses-slice';
+import { allExpenses, addExpense } from '../slices/expenses-slice';
+import { Expense, ExpenseInputsType } from '@/types';
+import axios from 'axios';
 
 export const getAllExpenses = () => async (dispatch: any) => {
-  const response = await fetch('http://127.0.0.1:3000/api/v1/expenses');
-  const data = await response.json();
-  data.amount = +data.amount;
-  dispatch(allExpenses(data));
+  await axios
+    .get('http://127.0.0.1:3000/api/v1/expenses')
+    .then(({ data }) => {
+      const expensesList: Expense[] = data;
+      dispatch(allExpenses(expensesList));
+    })
+    .catch((err) => console.log(err));
+};
+
+export const createExpense = (expense: ExpenseInputsType) => async (dispatch: any) => {
+  await axios
+    .post('http://127.0.0.1:3000/api/v1/expense', expense)
+    .then(({ data }) => {
+      dispatch(addExpense(data.expense));
+    })
+    .catch((err) => console.log(err));
 };
--- client/src/redux/slices/expenses-slice.ts
+++ client/src/redux/slices/expenses-slice.ts
@@ -2,29 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { Expense } from '@/types';
 
-const DUMMY_EXPENSES: Expense[] = [
-  { id: 'e1', description: 'A pair of shoes', amount: 59.99, date: 'Sat Jun 18 2022' },
-  { id: 'e2', description: 'Bananas', amount: 39.29, date: 'Saturday, 18 June 2022 05:44:30' },
-  { id: 'e3', description: 'Book', amount: 12.42, date: 'Saturday, 18 June 2022 05:44:30' },
-  { id: 'e4', description: 'Lightsaber', amount: 145.55, date: 'Saturday, 18 June 2022 05:44:30' },
-  { id: 'e5', description: 'Lego', amount: 421.12, date: 'Saturday, 18 June 2022 05:44:30' },
-  { id: 'e6', description: 'Coconut', amount: 16.21, date: 'Saturday, 18 June 2022 05:44:30' },
-  { id: 'e7', description: 'Iphone', amount: 369.12, date: 'Saturday, 18 June 2022 05:44:30' },
-  { id: 'e8', description: 'TV', amount: 4221.86, date: 'Saturday, 18 June 2022 05:44:30' },
-  {
-    id: 'e9',
-    description: 'Star Trek Movie',
-    amount: 9.23,
-    date: 'Saturday, 18 June 2022 05:44:30'
-  },
-  {
-    id: 'e10',
-    description: 'Chewbacca Cosplay Suit',
-    amount: 421.12,
-    date: 'Saturday, 18 June 2022 05:44:30'
-  }
-];
-
 const initialState: Expense[] = [];
 
 export const expensesSlice = createSlice({
