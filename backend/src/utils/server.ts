5357b1ceb79869cb41f21a5068b6c8c5a558ae63
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-07-11 18:13:21 +0200

    apply user to request

--- backend/src/utils/server.ts
+++ backend/src/utils/server.ts
@@ -1,11 +1,26 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import expenseRouter from '@/routes/expenses';
 import bodyParser from 'body-parser';
+
+import { User } from '@/models';
+
+const userMiddlewear = (req: Request, res: Response, next: NextFunction) => {
+  User.findByPk(1)
+    .then((user) => {
+      if (user) {
+        req.user = user;
+      }
+      next();
+    })
+    .catch((err) => console.log(err));
+};
 
 function createServer() {
   const app = express();
 
   app.use(bodyParser.json());
+
+  app.use(userMiddlewear);
 
   app.use('/api/v1', expenseRouter);
 
