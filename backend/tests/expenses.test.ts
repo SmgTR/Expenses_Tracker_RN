5322fec9d136309e8b6057cd1be9c71440e9e249
SmgTr <59412217+SmgTR@users.noreply.github.com>
2022-07-24 06:44:42 +0200

    add token to requests

--- backend/tests/expenses.test.ts
+++ backend/tests/expenses.test.ts
@@ -1,78 +1,32 @@
-import { User } from '@/models';
-import Expense from '@/models/expense';
+import { generateJWT } from '@/auth/jwtStrategy';
+
 import createServer from '@/utils/server';
-import passport from 'passport';
 import supertest from 'supertest';
 
-const dummyExpense = {
-  description: 'Macbook Pro 244',
-  amount: 233.4,
-  date: new Date(),
-  userId: 1,
-  save: jest.fn().mockImplementation(() => editedExpense)
-};
-
-const dummyUser = {
-  id: 1,
-  email: 'test@test.pl'
-};
-
-const editedExpense = {
-  description: 'California trip',
-  amount: 233.4,
-  date: new Date()
-};
-
-jest.mock('@/models/user', () => ({
-  findAll: jest.fn().mockImplementation(() => Promise.resolve(User)),
-  findByPk: jest.fn().mockImplementation(() => dummyUser),
-  findOne: jest.fn().mockImplementation(() => Promise.resolve(dummyUser as User)),
-  destroy: jest.fn(),
-  getExpenses: jest.fn().mockImplementation(async () => {
-    return await Promise.resolve(User).then(() => {
-      return [dummyExpense];
-    });
-  }),
-  createExpense: jest.fn().mockImplementation(({ amount, description, date }) => {
-    if (!amount || !description || !date) {
-      throw new Error();
-    } else {
-      return dummyExpense;
-    }
-  })
-}));
-
-// passport.authenticate = jest.fn(() => {
-//   return { id: 1, email: 'tester@test.pl' } as User;
-// });
-
-jest.mock('@/models/expense', () => ({
-  findAll: jest.fn(),
-  create: jest.fn().mockImplementation(({ amount, description, date }) => {
-    if (!amount || !description || !date) {
-      throw new Error();
-    } else {
-      return dummyExpense;
-    }
-  }),
-  findByPk: jest.fn().mockImplementation(() => Promise.resolve(Expense)),
-  save: jest.fn().mockImplementation(() => {
-    Promise.resolve(editedExpense);
-  }),
-  destroy: jest.fn()
-}));
+import { dummyUser, dummyExpense } from './testsData';
 
 const app = createServer();
-// try to get token, maybe not working cuz of unathorized
+
 describe('Manage expenses', () => {
+  let userToken: any;
+  beforeEach(() => {
+    userToken = generateJWT(dummyUser.email, dummyUser.id);
+  });
+
   describe('Get expense', () => {
     test('Send all expenses', async () => {
-      await supertest(app).get('/api/v1/expenses').expect(200);
+      await supertest(app)
+        .get('/api/v1/expenses')
+        .set('Authorization', 'Bearer ' + userToken)
+        .expect(200);
     });
   });
   describe('Create expense', () => {
     test('Create new expense', async () => {
-      const expense = await supertest(app).post('/api/v1/expense').send(dummyExpense);
+      const expense = await supertest(app)
+        .post('/api/v1/expense')
+        .set('Authorization', 'Bearer ' + userToken)
+        .send(dummyExpense);
       expect(expense.statusCode).toBe(201);
     });
 
@@ -80,15 +34,19 @@ describe('Manage expenses', () => {
       const bodyData = [{ description: 'description' }, { amount: 20 }, { date: '2022-06-12' }, {}];
 
       for (const body of bodyData) {
-        const response = await supertest(app).post('/api/v1/expense').send(body);
+        const response = await supertest(app)
+          .post('/api/v1/expense')
+          .set('Authorization', 'bearer ' + userToken)
+          .send(body);
         expect(response.statusCode).toBe(400);
       }
     });
   });
   describe('Edit expense', () => {
     test('Edit expense with correct data', async () => {
-      const response = await supertest(app)
+      await supertest(app)
         .put('/api/v1/expense?expenseId=5')
+        .set('Authorization', 'Bearer ' + userToken)
         .send({ ...dummyExpense })
         .expect(200);
     });
@@ -96,7 +54,10 @@ describe('Manage expenses', () => {
     test('Editing fails if incorrect data', async () => {
       const bodyData = [{ description: 'description' }, { amount: 20 }, { date: '2022-06-12' }, {}];
       for (const body of bodyData) {
-        const response = await supertest(app).put('/api/v1/expense').send(body);
+        const response = await supertest(app)
+          .put('/api/v1/expense')
+          .set('Authorization', 'Bearer ' + userToken)
+          .send(body);
         expect(response.statusCode).toBe(400);
       }
     });
@@ -104,13 +65,16 @@ describe('Manage expenses', () => {
     test('It saves edited data', async () => {
       const response = await supertest(app)
         .put('/api/v1/expense?expenseId=5')
+        .set('Authorization', 'Bearer ' + userToken)
         .send({ ...dummyExpense });
       expect(response.body.editedExpense.description).toMatch(/california trip/i);
     });
   });
   describe('Delete expense', () => {
     test('It deletes expense item', async () => {
-      const response = await supertest(app).delete('/api/v1/expense?expenseId=5');
+      const response = await supertest(app)
+        .delete('/api/v1/expense?expenseId=5')
+        .set('Authorization', 'Bearer ' + userToken);
       expect(response.statusCode).toBe(204);
     });
   });
